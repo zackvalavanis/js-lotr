813c4d494cd41448dd3a6337209b80fab1084408
dani <dani.zaghian@gmail.com>
2021-12-15 12:39:25 -0800

    separates starter tasks and solutions

--- scripts/solutions.js
+++ scripts/solutions.js
@@ -2,10 +2,12 @@
 // To make middle earth, write a function called makeMiddleEarth that returns the string "Hello Middle Earth!".
 
 // WRITE YOUR CODE HERE
-
+function makeMiddleEarth() {
+  return "Hello Middle Earth!";
+}
 
 // TESTING TASK 1
-// testMakeMiddleEarth();
+testMakeMiddleEarth();
 
 // Task 2: Put hobbits on the map
 // To add the hobbits to The Shire, write a function called takeAttendance that returns the length of the 'hobbits' array.
@@ -13,19 +15,23 @@
 var hobbits = ["Frodo Baggins", "Samwise 'Sam' Gamgee", "Meriadoc 'Merry' Brandybuck", "Peregrin 'Pippin' Took"];
 
 // WRITE YOUR CODE HERE
-
+function takeAttendance() {
+  return hobbits.length;
+}
 
 // TESTING TASK 2
-// testTakeAttendance(); 
+testTakeAttendance(); 
 
 // Task 3: Give Frodo the ring 
 // To give Frodo the ring, write a function called keepSecret that takes in a string parameter and returns that string in all lower case. Hint: look up a JavaScript string method for this.
 
 // WRITE YOUR CODE HERE
-
+function keepSecret(string) {
+  return string.toLowerCase();
+}
 
 // TESTING TASK 3
-// testKeepSecret();
+testKeepSecret();
 
 // try clicking on the ring to hear something earsplitting!
 
@@ -36,10 +42,16 @@ var hobbits = ["Frodo Baggins", "Samwise 'Sam' Gamgee", "Meriadoc 'Merry' Brandy
 var partySize = 4;
 
 // WRITE YOUR CODE HERE
-
+function doubleParty() {
+  partySize++;
+  partySize++;
+  partySize++;
+  partySize++;
+  return partySize;
+}
 
 // TESTING TASK 4
-// testDoubleParty();
+testDoubleParty();
 
 // click on Strider to see a transformation!
 
@@ -50,20 +62,26 @@ function travelAbroad() {
   var shire = ['Frodo Baggins', 'Samwise Gamgee', 'Merry Brandybuck', 'Pippin Took']
   var rivendell = []
   // WRITE YOUR CODE HERE
-
+  shire.forEach(hobbit => {
+    shire.unshift();
+    rivendell.push(hobbit);
+  })
+  return rivendell;
 }
 
 // TESTING TASK 5
-// testTravelAbroad();
+testTravelAbroad();
 
 // Task 6: Forge the fellowship
 // To forge the fellowship, write a function called declareAllegiance that takes in a member (a string, such as 'Sam') and concatenates it into a string to return "I, Sam, am part of the fellowship!"
 
 // WRITE YOUR CODE HERE
-
+function declareAllegiance(member) {
+  return "I, " + member + ", am part of the fellowship!";
+}
 
 // TESTING TASK 6
-// testDeclareAllegiance();
+testDeclareAllegiance();
 
 // click on Rivendell to hear a tune!
 
@@ -74,11 +92,12 @@ function travelAbroad() {
 
 function slayTheBalrog() {
   // WRITE YOUR CODE HERE
-  
+  var gandalf = "Gandalf";
+  return `${gandalf} the Grey says: You shall not pass! ${gandalf} the Grey is now ${gandalf} the white.`;
 }
 
 // TESTING TASK 7
-// testSlayTheBalrog();
+testSlayTheBalrog();
 
 // Task 8: Blow the Horn of Gondor
 // Observe the function with the Boromir object below. Have the function return the value of calling the blowHorn function on boromir. Docs: https://www.w3schools.com/js/js_object_methods.asp
@@ -93,11 +112,11 @@ function blowTheHornOfGondor() {
     }
   };
   // WRITE YOUR CODE HERE
-  
+  return boromir.blownHorn();
 }
 
 // TESTING TASK 8
-// testBlowTheHornOfGondor();
+testBlowTheHornOfGondor();
 
 // Task 9: Send Frodo and Sam to Mount Doom
 // To send Frodo and Sam to Mount Doom, find Frodo's place in the fellowshipMembers array in the function below using the indexOf method: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf. Save that value to a variable called frodosPlace. Then, use the .splice method to remove and return 2 elements from the fellowshipMembers array (Frodo and Sam) starting at frodosPlace: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice. The splice method has a return value, you can use that as your function return!
@@ -105,11 +124,12 @@ function blowTheHornOfGondor() {
 function itsDangerousToGoAlone() {
   var fellowshipMembers = ['Gandalf the White', 'Legolas', 'Gimli', 'Aragorn', 'Frodo Baggins', 'Samwise Gamgee', 'Merry Brandybuck', 'Pippin Took']
   // WRITE YOUR CODE HERE
-  
+  var frodosPlace = fellowshipMembers.indexOf('Frodo Baggins');
+  return fellowshipMembers.splice(frodosPlace, 2);
 }
 
 // TESTING TASK 9
-// testItsDangerousToGoAlone();
+testItsDangerousToGoAlone();
 
 // Task 10: Gollum takes the ring
 // To have Gollum take the ring, take a look at the code below and uncomment the noted lines to look at the console output. Then, read up on function scope and global scope here (you just need those 2): https://www.w3schools.com/js/js_scope.asp. Finally, fix the weWantsIt function to use the correct variable scope for the gollumHasRing variable so the global value changes to true.
@@ -118,25 +138,31 @@ var gollumHasRing = false;
 
 function weWantsIt() {
   console.log("Gollum has attempted to take the Ring from Frodo!");
-  var gollumHasRing = true;
+  gollumHasRing = true;
 }
 
 // UNCOMMENT THE FOLLOWING LINES
-// weWantsIt();
-// console.log(`gollumHasRing: ${gollumHasRing}`);
+weWantsIt();
+console.log(`gollumHasRing: ${gollumHasRing}`);
 
 // TESTING TASK 10
-// testWeWantsIt();  
+testWeWantsIt();  
 
 
 // Task 11: Vanquish Gollum and destroy the Ring
 // To send Gollum and the Ring into the lava of Mount Doom, write a function called wrestleGollum. The function should use a for loop (docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration) to loop 5 times and increase a variable
 
 // WRITE YOUR CODE HERE
-
+function wrestleGollum() {
+  var counter = 0;
+  for (let i = 0; i < 5; i++) {
+    counter++;
+  }
+  return counter;
+}
 
 // TESTING TASK 11
-// testWrestleGollum();
+testWrestleGollum();
 
 // Task 12: Send the hobbits back to the Shire
 // To send the hobbits all back to the shire, complete the thereAndBackAgain function below. Use the .filter array method to get a filtered array of only hobbits using the hobbit key (note the array is now filled with objects!). Array filter documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter. Then, loop through your filtered array of hobbits and change each one's location to 'The Shire'. Finally, return the array of filtered hobbits in their updated location.
@@ -153,8 +179,12 @@ function thereAndBackAgain() {
     {name: 'Pippin Took', hobbit: true, location: 'Rivendell'}
   ];
   // WRITE YOUR CODE HERE
-  
+  var hobbits = fellowshipMembers.filter(member => member.hobbit);
+  hobbits.forEach(hobbit => {
+    hobbit.location = 'The Shire';
+  });
+  return hobbits;
 }
 
 // TESTING TASK 12
-// testThereAndBackAgain();
+testThereAndBackAgain();
